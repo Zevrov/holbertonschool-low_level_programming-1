da88c4d08757dc31a83ba8c4ef2b466ee52863fd
Kevin Yook <yook00627@gmail.com>
2018-01-18 22:42:12 +0000

    00 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, i2;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (i2 = '0'; i2 <='9'; i2++)
+		{
+			putchar(i);
+			putchar(i2);
+			if (i2 < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
