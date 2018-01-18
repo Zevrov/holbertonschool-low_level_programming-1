0d7770057360393f62a62143712fe0cb7211ee13
Kevin Yook <yook00627@gmail.com>
2018-01-18 21:09:17 +0000

    spell check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Lst digit of %d is %d and is less than 6 and not 0\n", n, ln);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ln);
 	}
 	return (0);
 }
