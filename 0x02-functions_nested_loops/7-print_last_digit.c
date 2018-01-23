1ca54bca110383d8c0b43a8c570e36e35112b3cc
Kevin Yook <yook00627@gmail.com>
2018-01-23 18:16:22 +0000

    print last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ * @n: Value of input number
+ *
+ * Return: retur last digit
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = (n * -1) % 10;
+		_putchar(n + '0');
+		return (n);
+	}
+	else
+		n = n % 10;
+		_putchar(n + '0');
+		return (n);
+}
