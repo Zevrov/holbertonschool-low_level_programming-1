919b9e55fcf85e308d7e079a8414c18be5c4aef1
Kevin Yook <yook00627@gmail.com>
2018-01-23 21:37:51 +0000

    fix first numbers

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,28 +7,33 @@ void times_table(void)
 {
 	int a, b, ab, ab1, ab2;
 
-	for (a = 0; a <= 9; a++)
+	a = 0;
+	while (a <= 9)
 	{
-		for (b = 0; b <= 9; b++)
+		b = 0;
+		while (b <= 9)
 		{
 			ab = a * b;
 			ab1 = ab / 10;
 			ab2 = ab % 10;
-			if (ab1 == 0)
-			{
-				_putchar(' ');
-			}
+			if (b == 0)
+				_putchar('0');
 			else
 			{
-				_putchar(ab1 + '0');
-			}
+				if (ab1 == 0)
+					_putchar(' ');
+				else
+					_putchar(ab1 + '0');
 			_putchar(ab2 + '0');
+			}
 			if (b < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
+			b++;
 		}
 		_putchar('\n');
+		a++;
 	}
 }
