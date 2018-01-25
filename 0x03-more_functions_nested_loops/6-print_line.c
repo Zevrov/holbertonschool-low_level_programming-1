668f95f0a191bf1a4c88e646288e413ca1f32ab2
Kevin Yook <yook00627@gmail.com>
2018-01-25 17:51:03 +0000

    print lines

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a stright line
+ *
+ * @n: Value of first input
+ */
+void print_line(int n)
+{
+	int c;
+
+	if (n > 0)
+		for (c = 0; c < n; c++)
+			_putchar('_');
+	_putchar('\n');
+}
