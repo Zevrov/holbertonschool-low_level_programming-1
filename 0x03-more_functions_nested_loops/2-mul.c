5096fa18f2ce9f82a7ca0b32b924f5d560f2c461
Kevin Yook <yook00627@gmail.com>
2018-01-25 16:56:33 +0000

    multiplication

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiply numbers
+ *
+ * @a: Value of first input
+ *
+ * @b: value of second input
+ *
+ * Return: multiplied number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
