a378eb93f0056c3f828d31fbce38a546c8414d1d
Kevin Yook <yook00627@gmail.com>
2018-01-26 03:14:25 +0000

    Print number

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -4,34 +4,37 @@
 /**
  * print_number - check for numbers
  *
- * @c: Value of input character
+ * @n: Value of input number
  */
 void print_number(int n)
 {
-	int c_digit, ten_pow, a, num, print_n;
+	int c_digit, ten_pow, a, print_n;
+	int num = n;
+	int num_v = n;
 
-	num = 0;
+	c_digit = 0;
 	if (n < 0)
 	{
 		_putchar('-');
-		num = n * -1;
+		num_v = num_v * -1;
 	}
-	c_digit = 0;
 	while (num != 0)
 	{
 		num = num / 10;
 		c_digit++;
 	}
 	if (c_digit == 1)
-		_putchar(num + '0');
+		_putchar(num_v + '0');
+	else if (n == 0)
+		_putchar(num_v + '0');
 	else
 	{
 		ten_pow = 10;
 		for (a = 2; a < c_digit; a++)
-			ten_pow = ten_pow * 10;
-		while (c_digit > 1)
+			ten_pow *= 10;
+		while (c_digit >= 1)
 		{
-			print_n = num / ten_pow % 10;
+			print_n = (num_v / ten_pow) % 10;
 			_putchar(print_n + '0');
 			ten_pow = ten_pow / 10;
 			c_digit--;
