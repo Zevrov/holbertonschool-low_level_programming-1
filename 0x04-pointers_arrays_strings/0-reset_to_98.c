9edfeadfc708e57b0f8871ae2b6ad9e43f99400a
Kevin Yook <yook00627@gmail.com>
2018-01-29 22:52:16 +0000

    header and 98 battery

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check for numbers
+ *
+ * @c: Value of input character
+ *
+ * Return: 1 if number 0 if not
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
