f17e3fd014ac5cfe3a71cc14e7dae5d987ef7c7a
Kevin Yook <yook00627@gmail.com>
2018-01-29 23:55:13 +0000

    string length

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen - count the length of string
+ *
+ * @s: Value of input of string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for(length = 0; *s != '\0'; s++)
+		length++;
+	return (length);
+}
