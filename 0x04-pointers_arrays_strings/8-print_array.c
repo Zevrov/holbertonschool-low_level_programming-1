5a6333f876ffcc590d0c52092261a53589713f09
Kevin Yook <yook00627@gmail.com>
2018-01-30 03:01:42 +0000

    print array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints one char out of 2 of a string
+ *
+ * @a: Value of input array
+ *
+ * @n: Value of integer
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		printf("%d", a[count]);
+		if (count < n -1)
+			printf(", ");
+		else
+			printf("\n");
+		count++;
+	}
+}
