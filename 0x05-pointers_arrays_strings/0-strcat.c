a2e507e280c993cef688c228e6f2001f65031a19
Kevin Yook <yook00627@gmail.com>
2018-02-01 18:10:52 +0000

    segmentation fix

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strcat - append the string at the end
+ *
+ * @dest: output destination
+ *
+ * @src: input source
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, s_count = 0, f_count = 0;
+
+	while (dest[f_count] != '\0')
+		f_count++;
+	while (src[s_count] != '\0')
+		s_count++;
+	while (count <= s_count)
+	{
+		dest[f_count] = src[count];
+		f_count++;
+		count++;
+	}
+	return (dest);
+}
