d284b1b5fcd7290798c0d95bfcdb0189ae7150af
Kevin Yook <yook00627@gmail.com>
2018-02-05 22:56:25 +0000

    change break point

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -11,11 +11,15 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int s_count, a_count, count;
+	unsigned int s_count, a_count;
 
-	for (s_count = 0; s[s_count] != '\0' && s[s_count] != ','; s_count++)
+	for (s_count = 0; s[s_count] != '\0'; s_count++)
+	{
 		for (a_count = 0; accept[a_count] != '\0'; a_count++)
 			if (s[s_count] == accept[a_count])
-				count++;
-	return (count);
+				break;
+		if (accept[a_count] == '\0')
+			break;
+	}
+	return (s_count);
 }
