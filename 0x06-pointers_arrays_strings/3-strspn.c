0a2d4b8265aabb879a61e1166132ee65838fbed7
Kevin Yook <yook00627@gmail.com>
2018-02-05 22:13:15 +0000

    edge case space infront

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,11 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int s_count, a_count, count;
+	unsigned int s_count = 0, a_count, count;
 
-	for (s_count = 0; s[s_count] != '\0' && s[s_count] != ' '; s_count++)
+	while (s[s_count] < 33)
+	       s_count++;
+	for (; s[s_count] != '\0' && s[s_count] != ' '; s_count++)
 		for (a_count = 0; accept[a_count] != '\0'; a_count++)
 			if (s[s_count] == accept[a_count])
 				count++;
