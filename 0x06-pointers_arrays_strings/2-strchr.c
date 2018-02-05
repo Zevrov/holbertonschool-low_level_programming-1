1c95dc0467178633df260945b901b3059ed4a477
Kevin Yook <yook00627@gmail.com>
2018-02-05 19:29:09 +0000

    find character in string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locate character in a string
+ *
+ * @s: input pointer
+ *
+ * @c: input character
+ *
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		if (s[count] == c)
+		{
+			s = &s[count];
+			return (s);
+		}
+	}
+	s = &s[count];
+	return (s);
+}
