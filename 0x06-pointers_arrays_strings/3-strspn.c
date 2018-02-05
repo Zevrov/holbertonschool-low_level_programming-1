46c679c950c61f150952ff47b2ee7cc48170d473
Kevin Yook <yook00627@gmail.com>
2018-02-05 22:43:01 +0000

    change delimiter

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -13,10 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int s_count, a_count, count;
 
-	s_count = 0;
-	while (s[s_count] == ' ')
-		s_count++;
-	for (; s[s_count] != '\0' && s[s_count] != ' '; s_count++)
+	for (s_count = 0; s[s_count] != '\0' && s[s_count] != ','; s_count++)
 		for (a_count = 0; accept[a_count] != '\0'; a_count++)
 			if (s[s_count] == accept[a_count])
 				count++;
