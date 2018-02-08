c9573b633f69bf49a9706a0ee7e20b2e322f7ce3
Kevin Yook <yook00627@gmail.com>
2018-02-08 05:47:20 +0000

    fix edge case

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -64,10 +64,13 @@ int compare(char *s1, char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' || *s2 == '\0')
-		return (0);
-	else if(*s2 == '*' && *(end(s2) - 1) == '*')
-		return (0);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (compare(s1, s2));
+	}
+	if (*s2 == '*' && *(end(s2) - 1) == '*')
+		return (compare(s1, s2 + 1));
 	else if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	else
