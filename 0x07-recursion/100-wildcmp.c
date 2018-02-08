b844e3f2d96494a2abb8f90d169ca8f8345afea2
Kevin Yook <yook00627@gmail.com>
2018-02-08 04:43:47 +0000

    handle wild cards

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -66,6 +66,8 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
+	else if (*s2 == '*' && *(end(s2) - 1) == '*')
+		return (1);
 	else
 		return (compare(s1, s2));
 	return (0);
