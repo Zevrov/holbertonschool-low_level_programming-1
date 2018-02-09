2a280838c8d2a5f0db67caa8430639eda1fddfb1
Kevin Yook <yook00627@gmail.com>
2018-02-09 21:34:42 +0000

    multiplication of 2 numbers

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the multiplication of 2 arguments
+ *
+ * @argc: size of argv
+ *
+ * @argv: comandline verctor agruments
+ *
+ * Return: 1 if no arguments 0 esle
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
