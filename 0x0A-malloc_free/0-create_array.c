c570195dd80ccb940e9b4d5b63070f8826b9ddec
Kevin Yook <yook00627@gmail.com>
2018-02-13 21:00:46 +0000

    add another null case

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -18,6 +18,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
 	while (count < size)
 	{
 		str[count] = c;
