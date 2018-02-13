495bfef0369fd7e2495f16813b5e9c42fb7d813b
Kevin Yook <yook00627@gmail.com>
2018-02-13 20:55:11 +0000

    add legth function

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
  * _strdup - duplicate string to another array
@@ -12,8 +11,10 @@
 char *_strdup(char *str)
 {
 	char *s;
-	int c = 0, len = strlen(str) + 1;
+	int c = 0, len = 0;
 
+	while (str[len] != '\0')
+		len++;
 	if (str == NULL)
 		return (NULL);
 	s = malloc(sizeof(char) * len);
