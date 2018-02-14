5bf0c498b7702c9648a8408f0b49af2b3d952d11
Kevin Yook <yook00627@gmail.com>
2018-02-14 03:17:05 +0000

    print string concat

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - concatenate all the string input
+ *
+ * @ac: count arguments
+ *
+ * @av: input of arguments
+ *
+ * Return: grid or Null
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int count_a = 0, c1 = 0, c2 = 0, c = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (c1 < ac)
+	{
+		c2 = 0;
+		while (av[c1][c2] != '\0')
+		{
+			count_a++;
+			c2++;
+		}
+		c1++;
+	}
+	printf("%d", count_a);
+	str = malloc(sizeof(char) * count_a);
+	if (str == NULL)
+		return (NULL);
+	for (c1 = 0; c1 < ac; c1++)
+	{
+		for (c2 = 0; av[c1][c2] != '\0'; c2++)
+		{
+			str[c] = av[c1][c2];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
+}
