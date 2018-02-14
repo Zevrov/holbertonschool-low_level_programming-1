0c918d5eb15af7c2ae06796a0434e7f6002481f2
Kevin Yook <yook00627@gmail.com>
2018-02-14 00:00:51 +0000

    space

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		grid[c] = malloc(sizeof(int) * width);
 		if (grid[c] == NULL)
 		{
-			while(c >= 0)
+			while (c >= 0)
 			{
 				free(grid[c]);
 				c--;
