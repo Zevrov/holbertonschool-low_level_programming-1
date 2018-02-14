c11d261be0914c05f3ad378b302ec63a30453df3
Kevin Yook <yook00627@gmail.com>
2018-02-14 00:40:52 +0000

    fix betty

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -4,16 +4,18 @@
 /**
  * free_grid - free all 2 dimensional grid
  *
- * @gird: input matrix
+ * @grid: input matrix
  *
  * @height: input integer height
  */
 void free_grid(int **grid, int height)
 {
-	while(0 <= height)
+	int c = 0;
+
+	while (c <= height)
 	{
-		free(grid[height]);
-		height--;
+		free(grid[c]);
+		c++;
 	}
 	free(grid);
 }
