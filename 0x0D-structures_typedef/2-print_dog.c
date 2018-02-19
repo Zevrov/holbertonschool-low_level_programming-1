e3f749fa2813764ee9ddafe275027f210bc3154b
Kevin Yook <yook00627@gmail.com>
2018-02-19 22:28:15 +0000

    print the dogs

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print dog info
+ *
+ * @d: input dogs
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if ((*d).name != NULL)
+			printf("Name: %s\n", (*d).name);
+		else
+			printf("Name: (nil)\n");
+		if ((*d).age != 0)
+			printf("Age: %.6f\n", (*d).age);
+		else
+			printf("Name: (nil)\n");
+		if ((*d).owner != NULL)
+			printf("Name: %s\n", (*d).owner);
+		else
+			printf("Name: (nil)\n");
+	}
+}
