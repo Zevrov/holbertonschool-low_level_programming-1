9b25fdfb32832426d0109cef44a109d484db2f3e
Kevin Yook <yook00627@gmail.com>
2018-02-21 22:49:06 +0000

    chnage if statement

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -17,10 +17,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int c;
 
+	if (cmp != NULL)
+		return;
 	if (size <= 0)
 		return (-1);
 	for (c = 0; c < size; c++)
-		if (cmp(array[c]) != 0)
+		if (cmp(array[c]))
 			return (c);
 	return (-1);
 }
