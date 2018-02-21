181d35d2935c14627918817318bcf37899a70ab0
Kevin Yook <yook00627@gmail.com>
2018-02-21 22:54:19 +0000

    check for NULL function

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -18,11 +18,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int c;
 
 	if (cmp != NULL)
-		return;
-	if (size <= 0)
-		return (-1);
-	for (c = 0; c < size; c++)
-		if (cmp(array[c]))
-			return (c);
+	{
+		if (size <= 0)
+			return (-1);
+		for (c = 0; c < size; c++)
+			if (cmp(array[c]))
+				return (c);
+	}
 	return (-1);
 }
