7c1cfe5065b34014b13123b4e9ad228c081f078e
Kevin Yook <yook00627@gmail.com>
2018-02-23 19:54:15 +0000

    print string

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - print strings
+ *
+ * @separator: input number of arguments
+ *
+ * @n: Return the sum of all arguments
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int c;
+	char *s;
+	char *nil = "(nil)";
+
+	if (n == 0)
+		return;
+	va_start(list, n);
+	for (c = 0; c < n; c++)
+	{
+		s = va_arg(list, char *);
+		if (s != NULL)
+		{
+			printf("%s", s);
+		}
+		else
+		{
+			printf("%s", nil);
+		}
+		if (separator != NULL && c < n - 1)
+			printf("%s",separator);
+	}
+	printf("\n");
+	va_end(list);
+}
