9a811e958c8a92519bc48491e74f79f596e8a158
Kevin Yook <yook00627@gmail.com>
2018-03-01 19:06:40 +0000

    printing list initial

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *c_list = h;
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (c_list != NULL)
+	{
+		if (c_list->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", c_list->len, c_list->str);
+		c_list = c_list->next;
+		count++;
+	}
+	return (count);
+}
