58ccd7a5594ab6311a5137ae9c90f4be6a27207c
Kevin Yook <yook00627@gmail.com>
2018-03-05 19:27:46 +0000

    delete the first node

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the first node
+ *
+ * @head: input head address
+ *
+ * Return: return head node data
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *next_n;
+
+	if (head == NULL)
+		return (0);
+	next_n = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = next_n;
+	return (n);
+}
