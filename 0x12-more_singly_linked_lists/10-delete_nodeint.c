6f27322c242f4df49223e4c51a6c6f0eccf44a61
Kevin Yook <yook00627@gmail.com>
2018-03-05 23:12:01 +0000

    delete nodes

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+#include "lists.h"
+
+/**
+ * pop_listint - delete the first node
+ *
+ * @head: input head address
+ *
+ * Return: return head node data
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *next_n;
+
+	if (head == NULL)
+		return (0);
+	next_n = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = next_n;
+	return (n);
+}
+/**
+ * delete_nodeint_at_index - removing node at index
+ *
+ * @head: input head address
+ *
+ * @index: input index
+ *
+ * Return: 1 if succes -1 if fail
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *n_head;
+	listint_t *holder;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+		return (pop_listint(head));
+	count = 1;
+	n_head = *head;
+	while (count < index)
+	{
+		if (n_head->next == NULL)
+			return (-1);
+		n_head = n_head->next;
+		count++;
+	}
+	holder = n_head->next;
+	n_head->next = holder->next;
+	free(holder);
+	return (1);
+}
