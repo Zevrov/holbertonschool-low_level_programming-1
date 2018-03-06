5795d940258530681793749a1067ba232ba7387d
Kevin Yook <yook00627@gmail.com>
2018-03-06 19:08:00 +0000

    return new

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,24 +23,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	n_head = *head;
 	while (count < idx)
 	{
+		n_head = n_head->next;
 		if (n_head == NULL)
 			return (NULL);
-		n_head = n_head->next;
 		count++;
 	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	new->next = NULL;
 	if (idx == 0)
 	{
 		new->next = n_head;
 		*head = new;
+		return (*head);
 	}
 	else
 	{
 		new->next = n_head->next;
 		n_head->next = new;
 	}
-	return (*head);
+	return (new);
 }
