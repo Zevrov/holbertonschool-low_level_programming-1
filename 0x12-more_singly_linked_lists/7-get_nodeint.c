485a8d65089541ae293f4f5511009d103d992d57
Kevin Yook <yook00627@gmail.com>
2018-03-06 18:06:23 +0000

    check if index is larger

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -5,6 +5,8 @@
  *
  * @head: input head address
  *
+ * @index: input of index
+ *
  * Return: reutrn the value of the node in the specific index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -15,6 +17,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	while (n < index)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 		n++;
 	}
