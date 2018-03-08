8be7b2a9b1b35802ae9fd229579ccdc2e98eb879
Kevin Yook <yook00627@gmail.com>
2018-03-08 20:42:45 +0000

    printing binary numbers

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_binary - convert decimals to bianry and print it
+ *
+ * @n: input decimal number unsinged long int
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 4611686018427387904;
+	int t = 0;
+
+	if (n == 0)
+		_putchar('0');
+	while (mask > 0)
+	{
+		if ((n & mask) == 0 && t != 1)
+		{
+			mask = mask >> 1;
+			continue;
+		}
+		if (n & mask)
+		{
+			t = 1;
+			_putchar('1');
+		}
+		else
+			_putchar('0');
+		mask = mask >> 1;
+	}
+}
