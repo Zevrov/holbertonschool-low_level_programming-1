043a1edf2c43b57b4382dd438a29357f08dd711f
Kevin Yook <yook00627@gmail.com>
2018-03-13 21:49:17 +0000

    create copy of files:

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * main - copy the file
+ *
+ * @argc: input number of arguments
+ *
+ * @argv: input pointer to strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int check, fd1, fd2;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n")
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n",
+			argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n",
+			argv[2]);
+		exit(99);
+	}
+	while ((check = read(fd1, buf, 1024)) > 0)
+		if (write(fd2, buf, check) != check)
+		{
+			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n",
+				argv[2]);
+			exit(99);
+		}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+}
