3a27bed694d639f30ed52582fed72e8a191bcdfd
Kevin Yook <yook00627@gmail.com>
2018-03-13 19:21:19 +0000

    create empty folder when null

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,7 +19,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		write(fd, "", 1);
+	{
+		close(fd);
+		return (1);
+	}
 	else
 	{
 		while (text_content[count] != '\0')
