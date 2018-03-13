526825255f67fe62ec3cd103d35724c68372128e
Kevin Yook <yook00627@gmail.com>
2018-03-13 23:26:07 +0000

    check outside of the loop

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -45,6 +45,11 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(fd1) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
