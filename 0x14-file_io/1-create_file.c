fafc9718c7eb402e019193dc938f8755a5ffb055
Kevin Yook <yook00627@gmail.com>
2018-03-13 19:09:53 +0000

    create a file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * create_file - create a file with the text content
+ *
+ * @filename: input name of the file in string
+ *
+ * @text_content: input text of content in string
+ *
+ * Return: 1 on succes -1 if fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, num = 1, count = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		write(fd, "", 1);
+	else
+	{
+		while (text_content[count] != '\0')
+		{
+			count++;
+		}
+		num = write(fd, text_content, count);
+		if (num == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
