5c713c8cfd0a9a85ac4619b95d1fa1e4924a5823
Kevin Yook <yook00627@gmail.com>
2018-04-14 19:47:46 +0000

    get node at index

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *holder;
 	unsigned int n = 0;
 
-	if (head == NULL || index < 0)
+	if (head == NULL)
 		return (NULL);
 	holder = head;
 	while (holder != NULL && n < index)
