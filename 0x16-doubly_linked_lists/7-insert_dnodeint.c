ef44af3f953f40218ccf026ef5832647978d7752
Kevin Yook <yook00627@gmail.com>
2018-04-14 20:14:00 +0000

    change the code to stop at the node instead preveoouse

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,39 +14,37 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *holder, *new;
-	unsigned int c = 1;
+	unsigned int c = 0;
 
 	if (h == NULL)
 		return (NULL);
-	if ((*h == NULL && idx == 0) || idx == 0)
+	if (idx == 0)
 	{
 		holder = add_dnodeint(h, n);
 		return (holder);
 	}
-	if (*h != NULL)
+	holder = *h;
+	while (holder != NULL && c < idx)
 	{
-		holder = *h;
-		while (holder->next != NULL && c < idx)
-		{
-			holder = holder->next;
-			c++;
-		}
-		if (holder->next != NULL)
-		{
-			new = malloc(sizeof(dlistint_t));
-			if (new == NULL)
-				return (NULL);
-			new->n = n;
-			new->prev = holder;
-			new->next = holder->next;
-			holder->next = new;
-			return (new);
-		}
-		else if (c == idx)
-		{
-			holder = add_dnodeint_end(h, n);
-			return (holder);
-		}
+		holder = holder->next;
+		c++;
+	}
+	if (holder != NULL)
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		holder->prev->next = new;
+		new->prev = holder->prev;
+		new->next = holder;
+		holder->prev = new;
+		return (new);
+	}
+	else if (c == idx)
+	{
+		holder = add_dnodeint_end(h, n);
+		return (holder);
 	}
 	return (NULL);
 }
