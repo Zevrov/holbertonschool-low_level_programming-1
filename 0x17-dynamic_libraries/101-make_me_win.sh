4de9957b09349539bc17e49ffa6c8e96f36ded59
Kevin Yook <yook00627@gmail.com>
2018-04-18 20:39:15 +0000

    change the download

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
-#/bin/bash
-export LD_PRELOAD=$PWD/a.out
+#!/bin/bash
+wget -q -O /tmp/librand.so https://github.com/yook00627/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/librand.so
+export LD_PRELOAD=/tmp/librand.so
