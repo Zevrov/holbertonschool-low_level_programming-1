7b707e6dc60ce573e2da515516c6664e303a2c6e
Kevin Yook <yook00627@gmail.com>
2018-05-11 22:37:36 +0000

    hash table update

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(holder->key, key) == 0)
 		{
-			free(new->value);
+			free(holder->value);
 			holder->value = strdup(value);
 			return (1);
 		}
