5b1e192ecf70134c580678ff531dd64a76ec9691
Kevin Yook <yook00627@gmail.com>
2018-05-11 22:24:16 +0000

    hash table update

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -47,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new->next = NULL;
-	if (ht->array[index] == NULL)
+	if (ht->array[index] != NULL)
 		new->next = ht->array[index];
 	ht->array[index] = new;
 	return (1);
