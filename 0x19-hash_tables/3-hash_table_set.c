32d0edbbdd7277baa77c7c9e583fd700afcc7b37
Kevin Yook <yook00627@gmail.com>
2018-05-11 22:23:28 +0000

    hash table update

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -46,7 +46,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-	new->next = ht->array[index];
+	new->next = NULL;
+	if (ht->array[index] == NULL)
+		new->next = ht->array[index];
 	ht->array[index] = new;
 	return (1);
 }
