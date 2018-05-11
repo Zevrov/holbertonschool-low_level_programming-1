8035ee96c4bbb54e5460072eed871d416a3a05ba
Kevin Yook <yook00627@gmail.com>
2018-05-11 21:39:44 +0000

    hash table free

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -19,6 +19,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 	return (table);
 }
