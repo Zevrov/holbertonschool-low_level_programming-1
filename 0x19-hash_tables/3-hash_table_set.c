720b68cbea96f83f99277f063a042b43996bd0f0
Kevin Yook <yook00627@gmail.com>
2018-05-11 22:02:02 +0000

    hash table update

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,44 +13,41 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new;
+	unsigned long int index = 0;
+	hash_node_t *new = NULL;
 
 	if (ht == NULL || value == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+	if (ht->array[index] != NULL)
 	{
-		new = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		new->key = strdup(key);
-		if (new->key == NULL)
+		new = ht->array[index];
+		while (new != NULL && strcmp(new->key, key))
+			new = new->next;
+		if (new && strcmp(new->key, key) == 0)
 		{
-			free(new);
-			return (0);
+			free(new->value);
+			new->value = strdup(value);
 		}
-		new->value = strdup(value);
-		if (new->key == NULL)
-		{
-			free(new);
-			free(new->key);
-			return (0);
-		}
-		new->next = ht->array[index];
-		ht->array[index] = new;
 		return (1);
 	}
-	new = ht->array[index];
-	while (new != NULL && strcmp(new->key, key))
-		new = new->next;
-	if (new && strcmp(new->key, key) == 0)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(new->value);
-		new->value = strdup(value);
+		free(new);
+		return (0);
 	}
+	new->value = strdup(value);
+	if (new->key == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
 	return (1);
-
-
-
 }
