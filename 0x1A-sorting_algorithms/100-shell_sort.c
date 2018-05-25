281bc6545eb34991dc629c4603b98cf16ca3b45d
Kevin Yook <yook00627@gmail.com>
2018-05-25 21:37:48 +0000

    if check

--- 0x1A-sorting_algorithms/100-shell_sort.c
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -10,6 +10,8 @@ void shell_sort(int *array, size_t size)
 	size_t gap = 1, i, j;
 	int tmp;
 
+	if (size == 0 || size == 1)
+		return;
 	while (gap < size)
 		gap = gap * 3 + 1;
 	gap = (gap - 1) / 3;
