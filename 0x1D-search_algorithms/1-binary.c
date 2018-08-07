04e0678fed128f4b83d9566e6fcb872bf581e65c
Kevin Yook <yook00627@gmail.com>
2018-08-07 11:15:48 -0700

    binary search

---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * search -  searches for a value in an array using Binary search recursion
+ * @array: the input array to search integer
+ * @s: start of array
+ * @e: end of the array
+ * @value: value to search
+ * Return: first index where value is located or -1 if not found
+ */
+int search(int *array, size_t s, size_t e, int value)
+{
+	size_t c = 0, m = 0;
+	char delm = 0;
+
+	if (s > e)
+		return (-1);
+	printf("Searching in array:");
+	for (c = s; c <= e; c++)
+	{
+		printf("%c %d", delm, array[c]);
+		delm = ',';
+	}
+	printf("\n");
+	m = (s + e) / 2;
+	if (array[m] == value)
+		return (m);
+	if (array[m] < value)
+		return (search(array, m + 1, e, value));
+	return (search(array, s, m - 1, value));
+}
+
+
+/**
+ * binary_search -  searches for a value in an array using Binary search
+ * @array: the input array to search integer
+ * @size: size of array
+ * @value: value to search
+ * Return: first index where value is l///ocated or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return (search(array, 0, size - 1, value));
+}
